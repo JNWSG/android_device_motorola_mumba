11f9b300a6776e8b523fbec8cd062c837df87187
jmpfbmx <jmpf.bmx@gmail.com>
2025-01-03 14:15:10 +0100

    mumba: Add partition flags
    Change-Id: I0d863ea24997bc0c939432d1dafc8f4cab0db197
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- device.mk
+++ device.mk
@@ -6,6 +6,17 @@
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
+# Mountpoints/Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+PRODUCT_PACKAGES += \
+    vendor_bt_firmware_mountpoint \
+    vendor_dsp_mountpoint \
+    vendor_firmware_mnt_mountpoint \
+    vendor_fsg_mountpoint \
+    vendor_super_fsg_mountpoint \
+    vendor_super_modem_mountpoint
+
 # Shipping API
 BOARD_SHIPPING_API_LEVEL := 202404
 PRODUCT_SHIPPING_API_LEVEL := 36
